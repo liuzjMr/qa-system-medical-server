5e184670b9fa938202fa3bf873f0735927107222
lt <1763318259@qq.com>
2023-05-28 20:19:03 +0800

    项目初始化！

---
+++ src/main/java/com/lee/qasystemmedicalserver/QaSystemMedicalServerApplication.java
@@ -0,0 +1,13 @@
+package com.lee.qasystemmedicalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaSystemMedicalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaSystemMedicalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lee/qasystemmedicalserver/QaSystemMedicalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.qasystemmedicalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaSystemMedicalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
